445cdc8ba704bccc835d89208525492e93a69331
200198 <200198@HS_B213_D07.lfcs.local>
2014-10-29 19:28:07 -0400

    Autonomous

---
+++ src/edu/wpi/first/wpilibj/templates/commands/Autonomous.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands;
+import edu.wpi.first.wpilibj.templates.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Servo;
+/**
+ *
+ * @author 200198
+ */
+public class Autonomous extends CommandBase {
+    DigitalInput limitSwitch = new DigitalInput(1);
+    Servo servo = new Servo(1);
+    public Autonomous() {
+            requires (driveTrain);
+}
+    protected void initialize() {
+        servo.set(1);
+}
+    protected void execute() {
+        
+        if (limitSwitch.get()){
+            driveTrain.roboDrive(-0.5, 0);
+        }
+        else {
+            shooter.armForward();
+        }
+    }
+    protected boolean isFinished() {
+        return false;
+    }
+    protected void end() {
+        
+    }
+    protected void interrupted() {
+        driveTrain.roboDrive(0, 0);
+        shooter.armOff();
+    }
+}
